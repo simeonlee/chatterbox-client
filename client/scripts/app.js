862f4621c3b4fe981eb1348c62de10f527bbd2b6
bryanmojica <bryan@trestian.com>
2016-08-08 14:45:29 -0700

    XSS protection beginnings

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,47 @@
 // YOUR CODE HERE:
 
+
+var app = {
+  url: 'https://api.parse.com/1/classes/messages'
+};
+
+
+
+
+
+console.log(app);
+
+// fetch messages
+
+  var getThis = $.ajax({
+								    url: app.url,
+								    type: 'GET',
+								  
+								  });
+
+
+
+console.log(getThis);
+/*$(document).ready( function () {
+  $('.message-pane').click(function () {
+    $(this).slideUp();
+  });
+*/
+
+
+  // server request URL : 'https://api.parse.com/1/classes/messages'
+ /* $.ajax({
+  // This is the url you should use to communicate with the parse API server.
+  url: 'https://api.parse.com/1/classes/messages',
+  type: 'POST',
+  data: JSON.stringify(message),
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('chatterbox: Message sent');
+  },
+  error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message', data);
+  }
+	});*/
+
