69ccab51e19dacfbc9a10f07aef0d9ee54747bf5
simeonlee <simeonhlee@gmail.com>
2016-08-09 17:30:11 -0700

    Implement post solution lecture tweaking

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   app.fetch();
+  setTimeout(app.addMessages, 500);
 
   $('.message-btn').on('click', function() {
     app.handleSubmit();
@@ -19,7 +20,9 @@ $(document).ready(function() {
 // var url = 'https://api.parse.com/1/classes/messages';
 var message;
 var app = {
-  server: 'https://api.parse.com/1/classes/messages'
+  server: 'https://api.parse.com/1/classes/messages',
+  roomnames: {},
+  chatHistory: [],
 };
 
 app.init = function() {
@@ -46,7 +49,17 @@ app.send = function(message) {
   });
 };
 
-app.fetch = function() {
+var fetchMarker;
+var addMarker;
+
+var counter;
+
+app.fetch = function(cb) {
+  cb = cb || function (message) {
+    app.addMessage(message);
+    counter++;
+  };
+
   $.ajax({
     url: app.server,
     type: 'GET',
@@ -54,14 +67,33 @@ app.fetch = function() {
     // data: JSON.parse(data),
     success: function(data) {
       console.log(data);
+      
       var results = data.results;
-      for ( var i = results.length - 1; i >= 0; i-- ) {
+
+      for ( var i = 0; i < results.length; i++ ) {
         var message = results[i];
-        app.addMessage(message);
+        if ( fetchMarker === message.objectId ) {
+          break;
+        } else {
+          app.chatHistory.push(message);
+          // cb(message);
+          // app.addRoom(message);
+        }
       }
+      fetchMarker = results[0].objectId;
+
+      // app.addMessages();
     }
   });
 };
+
+/*success: function (data) {
+  for ( var i = 19; i >= 0; i-- ) {
+    var message = data.results[i];
+    var roomname = message.roomname;
+
+  }
+}*/
 
 
 app.clearMessages = function() {
@@ -69,9 +101,10 @@ app.clearMessages = function() {
 };
 
 app.addMessage = function(message) {
-  var username = message.username;
+  var username = escapeHtml(message.username);
   var text = escapeHtml(message.text);
   var time = escapeHtml(message.createdAt);
+  var roomname = escapeHtml(message.roomname);
 
   var $chats = $('#chats');
 
@@ -79,33 +112,141 @@ app.addMessage = function(message) {
   var $username = $('<div>', {class: 'username'});
   var $text = $('<div>', {class: 'chat chat-text'});
   var $time = $('<div>', {class: 'chat chat-time'});
+  var $roomname = $('<div>', {class: 'chat chat-roomname'});
 
   $username.text(username);
   $text.text(text);
   $time.text(calculateSince(time));
+  $roomname.text(roomname);
 
   $chat.append($username);
   $chat.append($text);
   $chat.append($time);
+  $chat.append($roomname);
   $chats.prepend($chat);
 
   $username.on('click', function() {
     app.addFriend(username);
   });
+
+  app.addRoom(message);
 };
 
-app.addRoom = function(roomName) {
-  var $roomSelect = $('#roomSelect');
-  var $room = $('<div>', {class: 'room'});
-  $room.text(roomName);
-  $roomSelect.append($room);
+
+app.addMessages = function(roomname) {
+
+  // Starts at Lobby upon page load, then we switch with a element click that originates from app.addRoom
+  roomname = roomname || 'Lobby';
+
+  // counter = 0;
+
+  // while ( counter < 20 ) {
+  for ( var i = app.chatHistory.length - 1; i >= 0; i-- ) {
+    var message = app.chatHistory[i];
+    if ( addMarker === message.objectId ) {
+      break;
+    } else {
+      // If the currentRoom is equal to Lobby, print all messages for now
+      if ( roomname === 'Lobby' ) {
+        app.addMessage(message);
+
+      // If the currentRoom is not equal to Lobby, it's clear we want to go to a different room
+      // go to that room if the roomname of the message is equal to the currentRoom that we pulled from the DOM
+      } else if ( roomname === message.roomname ) {
+        app.addMessage(message);
+      }
+      // app.addMessage(message);
+    }
+  }
+
+  addMarker = app.chatHistory[19].objectId;
+
+  // for ( var i = 0; i < results.length; i++ ) {
+  //       var message = results[i];
+  //       if ( fetchMarker === message.objectId ) {
+  //         break;
+  //       } else {
+  //         app.chatHistory.push(message);
+  //         // cb(message);
+  //         // app.addRoom(message);
+  //       }
+  //     }
+  //     fetchMarker = results[0].objectId;
 };
+
+var currentRoom;
+
+/*app.addRoom = function (message) {
+  var roomname = message.roomname;
+  if ( !app.roomnames[roomname] ) {
+    var $roomname = $('<div>', {class: 'roomname'});
+    $roomname.text(roomname);
+    $('#roomSelect').append($roomname);
+
+    $roomname.on('click', function () {
+      currentRoom = roomname;
+      $('.currentRoom').text(currentRoom);
+      app.clearMessages();
+      app.addMessages(roomname);
+    });
+  }
+
+  app.roomnames[roomname] = true;
+};*/
+
+app.addRoom = function (message) {
+  var roomname = message.roomname;
+  if ( !app.roomnames[roomname] ) {
+    var $roomname = $('<option>', {class: 'roomname', value: 'roomname', text: 'roomname'});
+    $roomname.text(roomname);
+    $('.chatRooms').append($roomname);
+
+    
+
+
+    /*$('.chatRooms option').on('click', function() {
+      $('.chatRooms').html($(this).class('selected'));
+    });
+
+    $('.chatRooms').find(":selected")($roomname);
+
+    $roomname.on('click', function () {
+      currentRoom = roomname;
+      $('.currentRoom').text(currentRoom);
+      app.clearMessages();
+      app.addMessages(roomname);
+    });*/
+
+    $roomname.on('click', function () {
+      currentRoom = roomname;
+      $('.currentRoom').text(currentRoom);
+      app.clearMessages();
+      app.addMessages(roomname);
+    });
+  }
+
+  app.roomnames[roomname] = true;
+};
+
+// var cb = function (message) {
+//   if ( message.roomname === currentRoom ) {
+//     app.addMessage(message);
+//     counter++;
+//   }
+// };
+// app.fetch(cb);
 
 app.addFriend = function(username) {
   var $newFriend = $('<div>', {class: 'friend friend-username'});
   $newFriend.text(username);
   $('#friends').append($newFriend);
+};
 
+app.displayFriendMsgs = function () {
+  $('.friend-username').on('click', function () {
+    $('#chats').empty();
+    // display only messages from friend
+  });
 };
 
 // Cross-Site Scripting (XSS) PROTECTION 
@@ -119,6 +260,7 @@ var entityMapping = {
   "/": '&#x2F;'
 };
 
+// NOTE: could have also used underbar's _.escape to accomplish this
 var escapeHtml = function(string) {
   return String(string).replace(/[&<>"'\/]/g, function(char) {
     return entityMapping[char];
